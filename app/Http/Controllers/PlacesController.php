3ce7c8fe966d286e8020203212c6135d4285e747
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2024-03-22 05:14:14 -0300

    Finished PlacesController

--- app/Http/Controllers/PlacesController.php
+++ app/Http/Controllers/PlacesController.php
@@ -2,12 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePlaceRequest;
+use App\Models\Place;
 use Illuminate\Http\Request;
 
 class PlacesController extends Controller
 {
+    public function index()
+    {
+        $places = Place::all();
 
-    public function index(){
-        dd("teste");
+        return $places; 
+    }
+
+    public function show(Request $request)
+    {
+        $place = Place::where('name', $request->name)->first();
+
+        if(!$place) return response()->json(['message' => 'Lugar nao encontrado!']);
+        
+        return $place;
+    }
+
+    public function store(StorePlaceRequest $request)
+    {
+        $data = $request->validated(); 
+
+        $newPlace = Place::create($data);
+
+        return $newPlace;
+    }  
+
+    public function update(StorePlaceRequest $request)
+    {    
+        $data = $request->validated();
+
+        $place = Place::findOrFail($request->id);
+
+        $place->update($data);
+        
+        return $place;
+    }
+
+    public function destroy(Request $request)
+    {   
+        Place::findOrFail($request->id)->delete();
+
+        return response()->json([],204);
     }
 }
